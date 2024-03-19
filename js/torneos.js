db4d0572573e223840acad76118a5c14c8328f38
cheppimdq <cheppimdp@gmail.com>
2024-03-19 19:25:49 -0300

    dns

---
+++ js/torneos.js
@@ -0,0 +1,36 @@
+const torneos = [
+    {
+    fecha: '12-19 de Marzo',
+    nombre: 'II Abierto Internacional de FAOGBA',
+    localidad: 'Ciudadela, Buenos Aires',
+    informacion: {
+        'Fecha': '12/3/2024 hasta 19/3/2024 (En juego)',
+        'Ritmo de juego': '90+30 (noventa minutos y treinta segundos de incremento por jugada)',
+        'Rondas': '9',
+        'Lugar de juego': 'Club Social y Deportivo NolTing, Alianza 350, Ciudadela, Buenos Aires',
+        'Organizador': 'FAOGBA',
+        'Federación': 'Argentina (ARG)',
+        'Director del Torneo': 'WGM Carolina Lujan',
+        'Árbitro principal': 'AI OI Leandro Plotinsky',
+        'Árbitro Adjunto Principal': 'AI Eugenia Alegre',
+        'Premios': '$1.000.000',
+        'Presencia estelar': 'Pepe Cuenca y Grandes Maestros como Diego Flores y Sandro Mareco',
+        'Sitio oficial': '<a href="https://internacionaldefaogba.wordpress.com/" target="_blank">Sitio oficial</a>',
+        'Inscripciones': '<a href="https://docs.google.com/forms/d/e/1FAIpQLSfnOGdzaTfPZTxIJBO5I_i-yhajUPX0gwjzOoZ_k6zCsv2b5Q/viewform" target="_blank">Formulario de inscripción</a>'
+    },
+    horarios: [
+        { ronda: '1', fecha: '2024/03/12', hora: '19:30' },
+        { ronda: '2', fecha: '2024/03/13', hora: '19:30' },
+        { ronda: '3', fecha: '2024/03/14', hora: '19:30' },
+        { ronda: '4', fecha: '2024/03/15', hora: '19:30' },
+        { ronda: '5', fecha: '2024/03/16', hora: '10:00' },
+        { ronda: '6', fecha: '2024/03/16', hora: '17:00' },
+        { ronda: '7', fecha: '2024/03/17', hora: '17:00' },
+        { ronda: '8', fecha: '2024/03/18', hora: '19:30' },
+        { ronda: '9', fecha: '2024/03/19', hora: '18:00' }
+    ]
+    },
+    // Aquí puedes agregar más torneos si es necesario
+];
+
+export default torneos;
