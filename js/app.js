88095d4cd16699dd5aacdd64d1836f0e35c66327
cheppimdq <cheppimdp@gmail.com>
2024-03-21 16:37:18 -0300

    nav transparente

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-//
+// SCROLL NAV
+window.addEventListener('scroll', function() {
+    var nav = document.getElementById('navTransparente');
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
